83c7ac0cb24df7f000748f5073ba122fa1f552d8
amin modaresi <aminiiiiiiiuii@gmail.com>
2022-05-03 23:23:31 +0430

    'Created SearchBox Component And Transferred Search Box Section to SearchBox Component'

---
+++ components/SearchBox.vue
@@ -0,0 +1,12 @@
+<template>
+    <section class="py-10">
+        <div class="container mx-auto">
+          <div class="py-5 flex justify-center shadow-lg">
+            <input type="text" class="border border-gray-300 mr-4 w-1/2 p-4 rounded outline-none text-xl" placeholder="Please Enter Movie Name">
+            <button class="border border-green-300 bg-green-400 text-gray-100 rounded p-4 outline-none">
+              Search
+            </button>
+          </div>
+        </div>
+      </section>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,13 @@
 <template>
   <div>
-      <section class="py-10">
-        <div class="container mx-auto">
-          <div class="py-5 text-center shadow-lg">
-            <input type="text" class="border border-gray-300 w-1/2 mx-auto p-4 rounded" placeholder="Please Enter Movie Name">
-            <button class="border border-green-300 bg-green-400 text-gray-100 rounded p-4">
-              Search
-            </button>
-          </div>
-        </div>
-      </section>
+      <SearchBox />
   </div>
 </template>
 
 <script>
+import SearchBox from "~/components/SearchBox.vue";
 export default {
-  name: 'IndexPage'
+    name: "IndexPage",
+    components: { SearchBox }
 }
 </script>
