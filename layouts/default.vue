ade5b63b40573abc2d19578f7ada43d8caf49d6a
amin modaresi <aminiiiiiiiuii@gmail.com>
2022-05-03 23:11:52 +0430

    Creata Search box

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,12 @@
 <template>
     <div>
-        <div>My blog navigation bar here</div>
+        <div class="shadow py-4">
+            <div class="container mx-auto">
+                <p class="text-lg">
+                    Move Search Info
+                </p>
+            </div>
+        </div>
         <Nuxt />
     </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    AS
+      <section class="py-10">
+        <div class="container mx-auto">
+          <div class="py-5 text-center shadow-lg">
+            <input type="text" class="border border-gray-300 w-1/2 mx-auto p-4 rounded" placeholder="Please Enter Movie Name">
+            <button class="border border-green-300 bg-green-400 text-gray-100 rounded p-4">
+              Search
+            </button>
+          </div>
+        </div>
+      </section>
   </div>
 </template>
 
